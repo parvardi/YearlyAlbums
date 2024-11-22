6e117cfdc26b2eadb1a06ad3bb675d02c72a52fe
Amir Parvardi <a.parvardi@gmail.com>
2024-11-22 15:10:50 -0800

    Reverting to deprecated st.experimental_set_query_params to check errors

--- YearlyAlbums.py
+++ YearlyAlbums.py
@@ -49,7 +49,7 @@ def get_url_parameters():
     """
     Retrieve URL query parameters using the updated st.query_params.
     """
-    return st.query_params()
+    return st.experimental_get_query_params()
 
 def authorize():
     """
@@ -109,14 +109,14 @@ def clear_query_params():
     """
     Clear query parameters from the URL after processing.
     """
-    st.query_params()
+    st.experimental_set_query_params()
 
 def logout():
     """
     Logout function to clear session state and revoke tokens.
     """
     st.session_state['spotify_cache'] = {}
-    st.query_params()
+    st.experimental_set_query_params()
     st.success("Logged out successfully!")
     st.experimental_rerun()
 
