e224c84c3e83def24635b8420007bbece2dcd7fb
Amir Parvardi <a.parvardi@gmail.com>
2024-11-22 21:01:35 -0800

    update query_parameters

--- YearlyAlbums_debug.py
+++ YearlyAlbums_debug.py
@@ -55,8 +55,8 @@ def get_url_parameters():
     """
     Retrieve URL query parameters using the updated st.query_params.
     """
-    # return st.query_params
-    return st.experimental_get_query_params()
+    return st.query_params
+    # return st.experimental_get_query_params()
 
 def authorize():
     """
